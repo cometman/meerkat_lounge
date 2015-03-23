40bf1e0ecc4f6baa5b5516f7fa3507b5ae445ad3
Clayton Selby <csbuiss@gmail.com>
2015-03-23 17:03:34 -0500

    meerkat update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User
   field :name, type: String
   field :id_str, type: String
   field :screen_name, type: String
+  field :profile_image_url, type: String
+  field :followers_count, type: Integer
+  field :location, type: String
+
   
   embedded_in :twitter
 end
